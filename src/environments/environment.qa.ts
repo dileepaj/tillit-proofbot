5f7e6887fc10f97b4d79dd72299cd209573b0539
bhawanthas <bpsshanuka@gmail.com>
2022-09-08 14:20:15 +0530

    TRAC-5835 first validate  txn hash and type

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -16,7 +16,7 @@ export const environment = {
                 getPococData: 'https://qa.gateway.tracified.com/proof/pococ/',
                 getPocData: 'https://qa.gateway.tracified.com/proof/poc/',
                 getPocTreeData: 'https://qa.gateway.tracified.com/pocv4/',
-                getTransactionData: 'https://qa.gateway.tracified.com/GetTransactions/',
+                getTransactionData: 'https://qa.gateway.tracified.com/GetTransactions',
                 getRecentTransactions: 'https://qa.gateway.tracified.com/RetrievePreviousTranasctions/',
                 getRecentTransactionsCount:'https://qa.gateway.tracified.com/RetrievePreviousTranasctionsCount/',
         }
